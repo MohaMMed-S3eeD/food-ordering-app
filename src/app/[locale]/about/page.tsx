8f143e9fdc174716362f492ac1227ba4b4e67124
Mohamed Seed <mosaeed7888@gmail.com>
2025-08-02 17:16:30 +0300

    Updated MainHeading component in About page to remove title and subtitle props for a cleaner presentation.

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <main className="section-gap">
       <div className="container">
-        <MainHeading title="About" subTitle="About" />
+        <MainHeading />
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"></div>
       </div>
     </main>
